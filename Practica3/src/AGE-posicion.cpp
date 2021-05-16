ebb7fe414043fbfae8efc917831d69d635c4f5dc
MarioCarmona <mcs2000carmona@gmail.com>
2021-05-16 12:20:00 +0200

    Terminada P2

---
+++ Practica3/src/AGE-posicion.cpp
@@ -0,0 +1,101 @@
+/**
+ * @file AGE-posicion.cpp
+ * @author Mario Carmona Segovia (mcs2000carmona@correo.ugr.es)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @file AGG-posicion.cpp
+ * @author Mario Carmona Segovia (mcs2000carmona@correo.ugr.es)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <funciones_aux.h>
+#include <timer.h>
+
+using namespace std;
+
+
+double AGG_posicion(list<Individuo> poblacionIni, const MatDouble* distancias, int numGenesFactible, float probabilidadCruce, float probabilidadMutacion) {
+
+    int iteraciones = 0;
+    int tamNuevaPoblacion = 2;
+
+    list<Individuo> poblacionActual = poblacionIni;
+
+    while(iteraciones < 100000) {
+        // Mecanismo de selección, en este modelo se realiza la selección y se
+        // obtiene una nueva población
+        list<Individuo> nuevaPoblacion;
+        operador_seleccion(&poblacionActual, &nuevaPoblacion, tamNuevaPoblacion);
+
+        // Operador de cruce
+        operador_cruce_posicion(&nuevaPoblacion, probabilidadCruce);
+
+        // Operador de mutación
+        operadorMutacion(&nuevaPoblacion, probabilidadMutacion);
+
+        // Cálculo del fitness de la población
+        int incre_iter = calcularFitness(&nuevaPoblacion, distancias);
+        // Mecanismo de reemplazo
+        operador_reemplazo(&poblacionActual, &nuevaPoblacion);
+
+        iteraciones += incre_iter;
+    }
+
+    return poblacionActual.front().fitness;
+}
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 3) {
+        cout << "Nº de argumentos incorrecto" << endl;
+        return -1;
+    }
+
+    // Semilla
+    int seed = atoi(argv[1]);
+
+    // Fichero con los datos de entrada
+    char* fichero = argv[2];
+
+    int numElemSelec;
+    MatDouble distancias;
+
+    // Obtenemos los datos de entrada
+    leerArchivo(fichero, numElemSelec, distancias);
+
+    srand(seed);
+
+    int numCromosomas = 50;
+    float probabilidadCruce = 1.0;
+    float probabilidadMutacion = 0.1/(distancias.size()+1);
+
+    list<Individuo> poblacionIni;
+    for(int i = 0; i < numCromosomas; ++i) {
+        poblacionIni.push_back(Individuo(distancias.size()+1, numElemSelec, &distancias));
+    }
+
+    poblacionIni.sort(compare_mayorFitness);
+
+    start_timers();
+
+    double fitness = AGG_posicion(poblacionIni, &distancias, numElemSelec, probabilidadCruce, probabilidadMutacion);
+
+    auto tiempo_ejec = elapsed_time();
+
+    cout << "Tiempo de ejecución: " << tiempo_ejec << endl;
+    cout << "Fitness: " << fitness << endl;
+
+    return 0;
+}
